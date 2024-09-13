6766dd5a4cdf50cc9d03941a1db503167d6d8d0f
Sohail Shaikh <50219669+Sohail0499@users.noreply.github.com>
2024-09-13 19:01:42 +0530

    Delete Diwali_Sales_Data_Analysis/Diwali_Sales.py

--- Diwali_Sales_Data_Analysis/Diwali_Sales.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib 
-import seaborn as sns
-
-s = pd.read_csv('Sohail0499/SS_Data_Analyst_Projects/Diwali_Sales_Data_Analysis/Diwali Sales Data.csv', encoding = 'unicode_escape')
-
-ss.shape
